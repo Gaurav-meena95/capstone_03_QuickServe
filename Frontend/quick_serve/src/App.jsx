cf70503b87d3ca13d00872053efe3fa0fe50d465
Gaurav Meena <gaurav_meena@Gauravs-MacBook-Pro.local>
2025-11-02 22:40:36 +0530

    1st end

--- Frontend/quick_serve/src/App.jsx
+++ Frontend/quick_serve/src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
-import { LoginPage } from './components/LoginPage'
-import { SplashScreen } from './components/SplashScreen'
 import { SignupPage } from './components/SignupPage'
 
 function App() {
   const [showSplash, setShowsplash] = useState(true)
+  const [currentPage, SetCurrentPage] = useState('login')
+  const [userRole, setUserRole] = useState(null);
+
+  const handleLogin = (userRole) => {
+    setUserRole(role)
+    if (role === 'customer') {
+      SetCurrentPage("customer-home")
+    } else if (role === 'shopkeeper') {
+      SetCurrentPage("shopkeeper-dashbord")
+    }
+  }
 
   // if (showSplash) {
   //   return <SplashScreen onComplete={() => setShowsplash(false)} />
@@ -13,7 +22,6 @@ function App() {
     <div className='size-full bg-slate-900 overflow-hidden'>
       {/* <SplashScreen/> */}
       {/* <LoginPage/> */}
-      <SignupPage/>
     </div>
   )
 }
