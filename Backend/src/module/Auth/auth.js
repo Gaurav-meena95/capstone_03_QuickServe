d0eedd0fc529bae2c08ef664068dd3af1d8c38f0
Gaurav Meena <gaurav_meena@Gauravs-MacBook-Pro.local>
2025-11-07 16:39:38 +0530

    jwt token done with bcrypt

--- Backend/src/module/Auth/auth.js
+++ Backend/src/module/Auth/auth.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router  = express.Router()
-const {signup} = require('./controle')
+const {signup, login} = require('./controle')
 
 router.post('/signup',signup) 
+router.post('/login',login)
 
 module.exports = router
 
--- Backend/src/module/Auth/controle.js
+++ Backend/src/module/Auth/controle.js
@@ -1,28 +1,61 @@
 const prisma = require('../../config/prismaClient')
-const { use } = require('./auth')
+const jwt  = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const sec_key = process.env.sec_key
+
 async function signup(req, res) {
     try {
         const { name, email, phone, password, role } = req.body
         if (!name || !email || !password || !role || !phone) {
             return res.status(401).json({ 'Error': 'All filed Required' })
         }
+        if(!email.includes('@') || !email.includes('.com')){
+            return res.status(401).json({message : "Invalid Email Address"})
+        }
         const existing = await prisma.user.findUnique({ where: { email } })
         if (existing) {
             return res.status(400).json({ message: 'User already exists' })
         }
+        const hashedPass = await bcrypt.hash(password,10)
+        console.log(hashedPass)
         const user = await prisma.user.create({
             data: {
                 name: name,
                 email: email,
                 phone: phone,
-                password: password,
+                password: hashedPass,
                 role: role
             }
         })
+
         return res.status(201).json({ message: 'Signup successful' })
     } catch (error) {
         console.log(error)
         res.status(500).json({ 'error': 'Internal Server Error', error: error.message })
     }
 }
-module.exports = { signup }
+
+async function login(req, res) {
+    try {
+        const { email, password, role } = req.body
+        const existing = await prisma.user.findUnique({ where: { email } })
+        if (!existing) {
+            return res.status(404).json({ message: "User not found" })
+        }else{
+            const isPasswordMatch = bcrypt.compareSync(password,user.password)
+            if (isPasswordMatch){
+                const token = await jwt.sign({email} , sec_key)
+                return res.json({token})
+            }else{
+                 return res.status(401).json({ message: 'Invalid credentials' })
+            }
+
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Login Faild', 'error': error.message })
+    }
+
+}
+
+module.exports = { signup, login }
