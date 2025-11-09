7df51ecba856a6a305d3af1342ba97ccbf01d021
Gaurav Meena <gaurav_meena@Gauravs-MacBook-Pro.local>
2025-11-09 17:41:25 +0530

    middleware created

--- Backend/src/module/Auth/controle.js
+++ Backend/src/module/Auth/controle.js
@@ -5,22 +5,19 @@ const sec_key = process.env.sec_key
 
 async function signup(req, res) {
     try {
-        const specialCharter = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/',
-            ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~']
-
         const { name, email, phone, password, role } = req.body
         if (!name || !email || !password || !role || !phone) {
             return res.status(401).json({ 'Error': 'All filed Required' })
         }
-        if (!email.includes('@') || !email.includes('.com')) {
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
             return res.status(401).json({ message: "Invalid Email Address" })
         }
-        const hasSpecialChar = specialCharter.some(char => password.includes(char))
-        if (!hasSpecialChar || password.length < 8) {
-            return res.status(401).json({
-                message: "Password must contain at least one special character (!,@,#,<,>,*,^,%,...) and be at least 8 characters long"
+        if (!/(?=.*[!@#$%^&*])(?=.{8,})/.test(password)) {
+            return res.status(400).json({
+                message: "Password must be at least 8 characters long and contain one special character"
             });
         }
+
         const existing = await prisma.user.findUnique({ where: { email, phone } })
         if (existing) {
             return res.status(400).json({ message: 'User already exists' })
@@ -46,14 +43,18 @@ async function signup(req, res) {
 async function login(req, res) {
     try {
         const { email, password, role, phone } = req.body
-        const existing = await prisma.user.findUnique({ where: { email, phone ,role} })
+        const existing = await prisma.user.findUnique({ where: { email, phone, role } })
 
         if (!existing) {
-            return res.status(404).json({ message: "User not found or Check your Role"})
+            return res.status(404).json({ message: "User not found or Check your Role" })
         } else {
             const isPasswordMatch = bcrypt.compareSync(password, existing.password)
-            if (isPasswordMatch  ) {
-                const jwtToken = await jwt.sign({ email }, sec_key)
+            if (isPasswordMatch) {
+                const jwtToken = await jwt.sign(
+                    { id: existing.id, email: existing.email, role: existing.role },
+                    sec_key,
+                    { expiresIn: "1h" }
+                )
                 return res.status(200).json({
                     message: "Login Successfully",
                     user: existing,
--- Backend/src/module/Auth/middleware.js
+++ Backend/src/module/Auth/middleware.js
@@ -1,7 +1,19 @@
-
+const jwt = require('jsonwebtoken')
 const authMiddleware = (req, res, next) => {
-    const { name, email, password, role } = req.body
-    if (role == "CUSTOMER"){
-        next()
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: "Access denied, no token provided" })
+    }
+    const token = authHeader.split(' ')[1]
+    try {
+        const decode = jwt.verify(token, process.env.sec_key)
+        req.user = decode;
+        next();
+
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ message: "Invalid or expired token" });
     }
 }
+module.exports = { authMiddleware }
